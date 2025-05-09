3f549d38f1a26bd6ba60268d42ed575d13c248b2
cold_nitrogen <niening_wamlngyi@163.com>
2025-05-09 17:43:16 +0800

    Fixed a bug with multiple calls of startCamera in recomposing.

--- app/src/main/java/cold/nitrogen/secretcamera/ui/screen/state/CameraUiState.kt
+++ app/src/main/java/cold/nitrogen/secretcamera/ui/screen/state/CameraUiState.kt
@@ -1,5 +1,10 @@
 package cold.nitrogen.secretcamera.ui.screen.state
 
+enum class CameraMode{
+    PHOTO, VIDEO
+}
+
 data class CameraUiState(
-    val isRecording: Boolean = false
+    val isRecording: Boolean = false,
+    val mode: CameraMode = CameraMode.PHOTO,
 )
